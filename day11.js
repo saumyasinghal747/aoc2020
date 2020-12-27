cad5d13953ff1e7bfa9c63c0a7980a1e1ef591ec
FrozenMango747 <realsaumya@gmail.com>
2020-12-26 22:07:05 -0800

    Updates

---
+++ day11.js
@@ -0,0 +1,11 @@
+const INPUT = require('./day11.json').SAMPLE
+//const {INPUT} = require('./day11.json')
+
+function main() {
+
+}
+
+module.exports = main;
+if (require.main === module) {
+    console.log(main())
+}
